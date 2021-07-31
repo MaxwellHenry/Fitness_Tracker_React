793458661d210f75eec9170afa4f43ab7c979376
Maxwell Montegary <maxwellmontegary@gmail.com>
2021-07-31 12:47:12 -0700

    Stashing

--- src/components/index.js
+++ src/components/index.js
@@ -7,4 +7,3 @@
 export { default as GetAllActivities } from './GetAllActivities';
 export { default as GetAllRoutines } from './GetAllRoutines';
 export { default as Homepage} from './Homepage'; 
-export { default as Homepage} from './Homepage'; 
